cbbe76c56677a94cd687a6cc6ba65e024eaf3f66
Shargrove09 <scottie@hopestreamlmft.com>
2022-12-09 17:58:38 -0500

    Added basic non-stylized UI to RPS game giving it a Pokemon Type spin. (Fire < Grass < Water)

--- rps.js
+++ rps.js
@@ -1,3 +1,7 @@
+let buttons = document.querySelectorAll(".button");
+let playerScore = 0;
+let computerScore = 0;
+
 function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -5,44 +9,79 @@ function randomInteger(min, max) {
 function getComputerChoice() {
   selection = randomInteger(1, 3);
 
-  if (selection == 1) return "rock";
-  else if (selection == 2) return "paper";
-  else return "scissors";
+  if (selection == 1) return "grass";
+  else if (selection == 2) return "fire";
+  else return "water";
 }
 
-function getPlayerChoice() {
-  let playerChoice = prompt("Type rock, paper or scissors");
-
+//Deprecated no longer needed
+/*
+function getPlayerChoice(playerChoice) {
+  // let playerChoice = prompt("Type grass, fire or water");
+  console.log("Player picked: ", playerChoice);
   return playerChoice;
-}
+} */
 
 function playRound(computerSelection, playerSelection) {
-  playerSelection = playerSelection.toLowerCase();
+  console.log("Comp: ", computerSelection);
+  console.log("Player: ", playerSelection);
+
   if (computerSelection == playerSelection) {
     return "TIE!";
   }
   // Player Wins
   else if (
-    (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "paper" && computerSelection == "rock") ||
-    (playerSelection == "scissors" && computerSelection == "paper")
+    (playerSelection == "grass" && computerSelection == "water") ||
+    (playerSelection == "fire" && computerSelection == "grass") ||
+    (playerSelection == "water" && computerSelection == "fire")
   ) {
+    playerScore++;
+    setPlayerScore();
     return `${playerSelection} beats ${computerSelection}. Player Wins!`;
   } else if (
-    (computerSelection == "rock" && playerSelection == "scissors") ||
-    (computerSelection == "paper" && playerSelection == "rock") ||
-    (computerSelection == "scissors" && playerSelection == "paper")
+    // Computer Wins
+    (computerSelection == "grass" && playerSelection == "water") ||
+    (computerSelection == "fire" && playerSelection == "grass") ||
+    (computerSelection == "water" && playerSelection == "fire")
   ) {
+    computerScore++;
+    setComputerScore();
     return `${computerSelection} beats ${playerSelection}. Computer Wins!`;
   } else {
-    console.log;
     return "Something has gone horribly wrong....";
   }
 }
 
 function game() {
+  // Play 5 Rounds
+  /*
   for (let i = 0; i < 5; i++) {
     console.log(playRound(getComputerChoice(), getPlayerChoice()));
+  }
+  */
+  //
+}
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const img = button.querySelector("img");
+    playerSelection = img.alt.toLowerCase();
+    console.log(playRound(getComputerChoice(), playerSelection));
+  });
+});
+
+function setPlayerScore() {
+  let playerScoreBox = document.querySelector("#player-score");
+  playerScoreBox.textContent = playerScore;
+  if (playerScore == 5) {
+    alert("PLAYER WINS");
+  }
+}
+
+function setComputerScore() {
+  let computerScoreBox = document.querySelector("#rival-score");
+  computerScoreBox.textContent = computerScore;
+  if (computerScore == 5) {
+    alert("Computer WINS");
   }
 }
 
